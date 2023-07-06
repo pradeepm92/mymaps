b885955c75670a68976d80a195f926a47efd33ce
Zeoner <pradeep.m@zeoner.com>
2023-07-06 17:55:04 +0530

    mapbox sdk issue fixed

--- app/src/main/java/com/example/mymaps/MapActivity.kt
+++ app/src/main/java/com/example/mymaps/MapActivity.kt
@@ -1,67 +1,51 @@
 package com.example.mymaps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.constants.Style
 import com.mapbox.mapboxsdk.maps.MapView
-import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+
 
 class MapActivity : AppCompatActivity() {
-    var mapview: MapView?= null
 
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Mapbox.getInstance(this, "sk.eyJ1IjoicHJhZGVlcDkyIiwiYSI6ImNsam1kbGZtbDAwMjAzZXFtYWZxams5dXMifQ.PUGXLPNdBjuzxwCCBW9NuQ")
-
+        Mapbox.getInstance(this, getString(R.string.token))
         setContentView(R.layout.activity_map)
-        mapview=findViewById(R.id.mapview)
-        mapview?.onCreate(savedInstanceState)
-        mapview?.getMapAsync(mapReadyCallback)
-    }
-    private val mapReadyCallback = OnMapReadyCallback { mapboxMap ->
-        // Perform map-related operations here
-        mapboxMap.setStyle(Style.MAPBOX_STREETS)
-    }
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync{MapboxMap->
+            MapboxMap.setStyle(Style.MAPBOX_STREETS){
+            }
+        }
 
-    // Other overridden methods such as onStart, onResume, etc.
+    }
 
     override fun onStart() {
         super.onStart()
-        mapview?.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapview?.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapview?.onPause()
+        mapView.onStart()
     }
 
     override fun onStop() {
         super.onStop()
-        mapview?.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapview?.onDestroy()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapview?.onSaveInstanceState(outState)
+        mapView.onStop()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        mapview?.onLowMemory()
+        mapView.onLowMemory()
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
 }
 
 
